cbfae8d59e19ab97a61498b5bd71ba74df04300e
Tur Tur Tur <kuba2323kuba@gmail.com>
2022-08-06 04:11:20 +0200

    made button animations

--- src/assets/styles/Theme.tsx
+++ src/assets/styles/Theme.tsx
@@ -7,17 +7,24 @@ const Theme = () => {
         body {
           font-family: "Montserrat", sans-serif;
           font-weight: 200;
+          width: 100vw;
+          height: 100vh;
           background-color: #1e5e76;
           color: #fff;
           user-select: none;
+        }
+        #root {
+          width: 100%;
+          height: 100%;
         }
         nav {
           width: 100%;
           height: 100px;
           display: flex;
           flex-direction: row;
-          justify-content: space-around;
+          justify-content: space-evenly;
           align-items: center;
+          flex-wrap: wrap;
         }
       `}
     />
--- src/components/nav/Button.tsx
+++ src/components/nav/Button.tsx
@@ -1,5 +1,35 @@
 import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
-const Button = styled.div``;
+const Button = styled(motion.div)`
+  position: relative;
+  padding: 15px 30px;
+  font-size: 1.4rem;
+  letter-spacing: 1.1rem;
+  text-transform: uppercase;
+  transition: all 500ms cubic-bezier(0.73, 0.08, 0.67, 1.32);
+  cursor: pointer;
+  user-select: none;
+
+  &::before {
+    content: "";
+    position: absolute;
+    transition: inherit;
+    z-index: -1;
+  }
+  &:hover::before {
+    left: 0;
+    width: 100%;
+  }
+  &::before {
+    top: 0;
+    left: 50%;
+    height: 100%;
+    width: 0;
+    border: 1px solid #e6e6e6;
+    border-left: 0;
+    border-right: 0;
+  }
+`;
 
 export default Button;
