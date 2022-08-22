0e9a1bd8577887cf54454688e3114cb1d75f2c8a
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-08-22 06:05:37 +0200

    updated vite config 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
